768a65b4d8f2b6d421018817f33f57e54f21428b
Qui <quipht@gmail.com>
2020-09-07 15:48:10 -0700

    update

---
+++ Tổng quan JavaScript 121/baitap1.js
@@ -0,0 +1,19 @@
+// Tính điểm trung bình
+// var VL=parseInt(prompt('Nhập điểm Vật lý:'));
+// var HH=parseInt (prompt('Nhập điểm Hóa học:'));
+// var SH=parseInt (prompt('Nhập điểm Sinh học:'));
+// var sub = VL + HH + SH;
+// var TB = sub / 3;
+// document.write("Điểm trung bình:" + TB); 
+// document.write("<br/>");
+// document.write("Tổng điểm:" + sub);
+// Chuyển độ C sang F
+// var C = parseInt (prompt('Nhập C:'));
+// var F = (C*1.8)+32;
+// // document.write('Độ F là:'+F);
+// Tính diện tích hình tròn và chu vi hình tròn
+var r= parseInt(prompt('Nhập bán kính:'));
+var S= (r*r)*3.14;
+var C= 2*r*3.14;
+document.write('Diện tích hình tròn='+S);
+document.write('Chu vi hình tròn ='+C);
